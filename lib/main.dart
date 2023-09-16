76c00a2ab9cac4a91b86e5a5005d3b61f7ce0d13
Ishaan Arya <66945981+ishaan-arya@users.noreply.github.com>
2023-09-16 18:09:12 -0400

    blue screen

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment(0.8, 1),
+
+          colors: <Color>[
+            Color(0xff62DCFA),
+            Color(0xff43CAE2),
+            Color(0xff1894B3),
+            Color(0xff11171C),
+          ], // Gradient from https://learnui.design/tools/gradient-generator.html
+          tileMode: TileMode.mirror,
+        ),
+      ),
+      child: Column(children: [
+        Text(
+          "Hello",
+          style: TextStyle(
+              color: Colors.white, // Set text color to white
+              fontWeight: FontWeight.bold,
+              fontSize: 32 // Set text to bold
+              ),
+        ),
+      ]),
+    ));
+  }
+}
