4a1978a711f6e9cdd332f4d0de57a0ad20c317b5
Ishaan Arya <66945981+ishaan-arya@users.noreply.github.com>
2023-09-16 23:12:40 -0400

    built the home screen

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const Color kPrimaryPurple = Color(0xff0ba8ff);
 const Color kPrimaryBlue = Color(0xffc687ff);
+const Color kPrimaryGrey = Color(0xff413D3D);
 
 const kTextFieldDecoration = InputDecoration(
   contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
--- lib/home.dart
+++ lib/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'constants.dart';
 import 'learn.dart';
 import 'test.dart';
+import 'package:glassmorphism_ui/glassmorphism_ui.dart';
 
 class HomePage extends StatelessWidget {
   @override
@@ -12,8 +13,8 @@ class HomePage extends StatelessWidget {
           decoration: BoxDecoration(
             gradient: LinearGradient(
               colors: <Color>[
-                kPrimaryBlue,
                 kPrimaryPurple,
+                kPrimaryBlue,
               ], // Change colors as needed
               begin: Alignment.topCenter,
               end: Alignment.bottomCenter,
@@ -24,55 +25,157 @@ class HomePage extends StatelessWidget {
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Padding(
-                  padding: EdgeInsets.only(left: 20, top: 80),
+                  padding: EdgeInsets.only(left: 30, top: 80),
                   child: Text(
                     "Hola, Lorem",
                     style: kHeadingTextStyle,
                   ),
                 ),
                 SizedBox(
-                  height: 35,
+                  height: 50,
                 ),
                 Padding(
                   padding: EdgeInsets.symmetric(horizontal: 20),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30),
                   child: Row(
                     children: [
-                      Container(
+                      GlassContainer(
                         height: 150,
                         width: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xff514D4D),
+                        blur: 8,
+                        color: Colors.white.withOpacity(1),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.white.withOpacity(0.5),
+                            Colors.blue.withOpacity(0.6),
+                          ],
                         ),
+                        //--code to remove border
+                        border: Border.fromBorderSide(BorderSide.none),
+                        shadowStrength: 8,
+                        borderRadius: BorderRadius.circular(16),
+                        shadowColor: Colors.black.withOpacity(0.5),
                         child: Padding(
-                          padding: EdgeInsets.only(top: 30, left: 50),
+                          padding: EdgeInsets.only(top: 30, left: 45),
                           child: Text(
                             "🇪🇸",
-                            style: TextStyle(fontSize: 40),
+                            style: TextStyle(fontSize: 50),
                           ),
                         ),
                       ),
                       SizedBox(
                         width: 20,
                       ),
-                      Container(
+                      GlassContainer(
                         height: 150,
                         width: 150,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Color(0xff514D4D),
+                        blur: 8,
+                        color: Colors.white.withOpacity(1),
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.white.withOpacity(0.5),
+                            Colors.blue.withOpacity(0.6),
+                          ],
                         ),
+                        //--code to remove border
+                        border: Border.fromBorderSide(BorderSide.none),
+                        shadowStrength: 8,
+                        borderRadius: BorderRadius.circular(16),
+                        shadowColor: Colors.black.withOpacity(0.5),
                         child: Padding(
-                          padding: EdgeInsets.only(top: 30, left: 50),
+                          padding: EdgeInsets.only(top: 30, left: 45),
                           child: Text(
                             "🇯🇵",
-                            style: TextStyle(fontSize: 40),
+                            style: TextStyle(fontSize: 50),
                           ),
                         ),
                       ),
                     ],
                   ),
                 ),
+                SizedBox(height: MediaQuery.sizeOf(context).height * 0.1),
+                GestureDetector(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    child: GlassContainer(
+                      height: 100,
+
+                      blur: 8,
+                      color: Colors.white.withOpacity(1),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.white.withOpacity(0.5),
+                          Colors.blue.withOpacity(0.6),
+                        ],
+                      ),
+                      //--code to remove border
+                      border: Border.fromBorderSide(BorderSide.none),
+                      shadowStrength: 8,
+                      borderRadius: BorderRadius.circular(16),
+                      shadowColor: Colors.black.withOpacity(0.5),
+                      child: Center(
+                        child: Text(
+                          "Learn",
+                          style: kHeadingTextStyle.copyWith(
+                            fontSize: 25,
+                            color: kPrimaryGrey,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/learn');
+                  },
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                GestureDetector(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 25),
+                    child: GlassContainer(
+                      height: 100,
+
+                      blur: 8,
+                      color: Colors.white.withOpacity(1),
+                      gradient: LinearGradient(
+                        begin: Alignment.topLeft,
+                        end: Alignment.bottomRight,
+                        colors: [
+                          Colors.white.withOpacity(0.5),
+                          kPrimaryPurple.withOpacity(0.6),
+                        ],
+                      ),
+                      //--code to remove border
+                      border: Border.fromBorderSide(BorderSide.none),
+                      shadowStrength: 8,
+                      borderRadius: BorderRadius.circular(16),
+                      shadowColor: Colors.black.withOpacity(0.5),
+                      child: Center(
+                        child: Text(
+                          "Test",
+                          style: kHeadingTextStyle.copyWith(
+                              fontSize: 25, color: kPrimaryGrey),
+                        ),
+                      ),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.pushNamed(context, '/test');
+                  },
+                )
               ],
             ),
           ),
